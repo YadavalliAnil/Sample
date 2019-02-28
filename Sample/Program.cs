8097f3a120739475394b9c7e8a1c73d90cebdeba
Anil Reddy Yadavalli <ayadavalli5@asiapac.globalcsc.net>
2019-02-28 12:03:37 +0530

    first commit

---
+++ Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Please enter a,b Values");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Result is" + c);
+            Console.ReadKey();
+        }
+    }
+}
